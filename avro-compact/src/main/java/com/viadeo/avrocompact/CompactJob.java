ff104a0fff638de34791328c64298f5d77f8201d
Jonathan Winandy <jwinandy@viadeoteam.com>
2014-02-26 12:09:53 +0100

    add compact job

---
+++ avro-compact/src/main/java/com/viadeo/avrocompact/CompactJob.java
@@ -0,0 +1,108 @@
+package com.viadeo.avrocompact;
+
+import org.apache.avro.Schema;
+import org.apache.avro.file.DataFileReader;
+import org.apache.avro.generic.GenericDatumReader;
+import org.apache.avro.mapred.FsInput;
+import org.apache.avro.mapreduce.AvroJob;
+import org.apache.avro.mapreduce.AvroKeyInputFormat;
+import org.apache.avro.mapreduce.AvroKeyOutputFormat;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.FileStatus;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+
+import java.io.IOException;
+
+
+public class CompactJob extends Configured implements Tool {
+
+    public static Schema getSchema(FileStatus fileStatus) throws Exception {
+        FsInput input = new FsInput(fileStatus.getPath(), new Configuration());
+        DataFileReader<Void> reader = new DataFileReader<Void>(input, new GenericDatumReader<Void>());
+        Schema schema = reader.getSchema();
+        reader.close();
+        input.close();
+        return schema;
+    }
+
+    public Job internalRun(Path inputDir, Path outputDir, Configuration conf) throws Exception {
+
+
+        conf.setBoolean("mapred.output.compress", true);
+
+
+        Job job = new Job(conf);
+        job.setJarByClass(CompactJob.class);
+        job.setJobName("compact");
+
+
+        FileSystem fileSystem = FileSystem.get(job.getConfiguration());
+        FileStatus[] inputFiles = fileSystem.globStatus(inputDir.suffix("/*.avro"));
+
+
+        if (inputFiles.length == 0) {
+            throw new Exception("At least one input is needed");
+        }
+
+        Schema schema = getSchema(inputFiles[0]);
+
+
+        FileInputFormat.setInputPaths(job, inputDir);
+        job.setInputFormatClass(AvroKeyInputFormat.class);
+
+        job.setMapperClass(Mapper.class);
+        AvroJob.setInputKeySchema(job, schema);
+        AvroJob.setMapOutputKeySchema(job, schema);
+        job.setMapOutputValueClass(NullWritable.class);
+        job.setReducerClass(Reducer.class);
+        AvroJob.setOutputKeySchema(job, schema);
+
+        job.setOutputValueClass(NullWritable.class);
+        job.setOutputFormatClass(AvroKeyOutputFormat.class);
+
+        // ~ OUTPUT
+        FileOutputFormat.setOutputPath(job, outputDir);
+
+
+        return job;
+    }
+
+    @Override
+    public int run(String[] args) throws Exception {
+        if (args.length != 2) {
+            System.err.println("Usage: Read <input path> <output path>");
+            return -1;
+        }
+
+        Configuration jobConf = getConf();
+
+        Path origPath = new Path(args[0]);
+        Path outPath = new Path(args[1]);
+
+        Job job = internalRun(origPath, outPath, jobConf);
+        boolean b = job.waitForCompletion(true);
+        if (!b) {
+            throw new IOException("error with job!");
+        }
+
+        return 0;
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        int res = ToolRunner.run(new Configuration(), new CompactJob(), args);
+        System.exit(res);
+    }
+
+
+}
---
+++ avro-compact/src/test/java/com/viadeo/avrodiff/CompactTest.java
@@ -0,0 +1,89 @@
+package com.viadeo.avrodiff;
+
+import java.io.File;
+
+import com.viadeo.avrocompact.CompactJob;
+import org.apache.avro.Schema;
+import org.apache.avro.file.DataFileReader;
+import org.apache.avro.file.DataFileWriter;
+import org.apache.avro.generic.GenericData.Record;
+import org.apache.avro.generic.GenericDatumReader;
+import org.apache.avro.generic.GenericDatumWriter;
+import org.apache.avro.generic.GenericRecord;
+import org.apache.avro.io.DatumReader;
+import org.apache.avro.io.DatumWriter;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import com.viadeo.avrodiff.GenerateSample.TestSchema;
+
+public class CompactTest {
+
+	@Rule
+	public TemporaryFolder tmpFolder = new TemporaryFolder();
+
+    public void create(String dirname, String filename, Record[] records) throws  Exception {
+
+
+        Schema schema = TestSchema.getSchema();
+
+        File dir = new File(tmpFolder.getRoot(), dirname);
+
+        if(!dir.exists() ){
+            dir.mkdirs();
+        }
+
+        File file = new File(dir,filename);
+        DatumWriter<GenericRecord> datumWriter = new GenericDatumWriter<GenericRecord>(schema);
+        DataFileWriter<GenericRecord> dataFileWriter = new DataFileWriter<GenericRecord>(datumWriter);
+        dataFileWriter.create(schema, file);
+        TestSchema.append(dataFileWriter,records);
+        dataFileWriter.close();
+
+    }
+
+
+    @Before
+    public void setUp() throws Exception {
+        Record[] records = {TestSchema.record("2", 2)};
+
+        create("a", "input.avro", records);
+
+    }
+
+
+    public static void assertContainsOnly(String base, Record record) throws Exception {
+        String path = base + "/part-r-00000.avro";
+
+        DatumReader<GenericRecord> datumReader = new GenericDatumReader<GenericRecord>();
+        DataFileReader<GenericRecord> dataFileReader = new DataFileReader<GenericRecord>(new File(path), datumReader);
+
+        //Read only one Record
+        GenericRecord user = dataFileReader.next();
+
+        Assert.assertEquals("should only contains ", user, record);
+
+    }
+
+
+	@Test
+	public void test42() throws Exception {
+
+		String outStr = tmpFolder.getRoot().getPath() + "/out-generic2";
+
+        Path diffin 	= new Path( new File(tmpFolder.getRoot(), "a").toURI().toString());
+        Path output 	= new Path(outStr);
+
+        Configuration jobConf = new Configuration();
+        CompactJob job = new CompactJob();
+        Assert.assertTrue(job.internalRun(diffin, output, jobConf).waitForCompletion(true));
+
+        String base = outStr + "/";
+        assertContainsOnly(base , TestSchema.record("2", 2));
+	}
+}
